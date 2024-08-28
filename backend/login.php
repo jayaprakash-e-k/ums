9e997e5d51f88de1e11fa134f4c5debd661199c2
jayaprakash-e-k <jp008882@gmail.com>
2024-08-28 13:29:51 +0530

    commit at 28.08.2024

--- backend/login.php
+++ backend/login.php
@@ -13,7 +13,22 @@ if (mysqli_num_rows($result) == 1) {
     $user = mysqli_fetch_assoc($result);
 
     if (password_verify($pass, $user['PASS'])) {
-        echo "Logged in!";
+        $role = $user['ROLE'];
+        session_start();
+
+        $_SESSION['user_id'] = $user['ID'];
+        $_SESSION['user_name'] = $user['NAME'];
+        $_SESSION['user_email'] = $user['EMAIL'];
+        $_SESSION['user_role'] = $user['ROLE'];
+        
+        if ($role == "user") {
+            header("Location: ../user/index.php");
+        } else if ($role == "admin"){
+            header("Location: ../admin/index.php");
+        } else {
+            echo "Role not defined";
+        }
+        exit();
     } else {
         echo "Username or Password Incorrect!";
     }
---
+++ user/index.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role']) || $_SESSION['user_role'] != "user") {
+    header("Location: ../loginForm.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome <?php echo $_SESSION['user_name']; ?>!</h1>
+
+    <a href="../logout.php">Logout</a>
+</body>
+</html>
